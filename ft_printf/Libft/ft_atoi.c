50a915d8efa649b9d8a3d6ef602490b6fe6aba2f
Gyeongsu Choi <gyechoi@c6r1s8.42seoul.kr>
2023-06-28 19:42:34 +0900

    atoi 변경

--- ft_printf/Libft/ft_atoi.c
+++ ft_printf/Libft/ft_atoi.c
@@ -11,16 +11,13 @@ int	ft_atoi(const char *str)
 	sign = 1;
 	while ((*str >= 9 && *str <= 13) || *str == 32)
 		str++;
-	if (*str == '+')
-		str++;
-	else if (*str == '-')
-	{
+	if (*str == '-')
 		sign = -1;
+	if (*str == '+' || *str == '-')
 		str++;
-	}
-	while (*str >= 48 && *str <= 57)
+	while (*str >= '0' && *str <= '9')
 	{
-		res = res * 10 + *str++ - 48;
+		res = res * 10 + *str++ - '0';
 		if (sign == 1 && res > long_max)
 			return (-1);
 		if (sign == -1 && res > long_max + 1)
